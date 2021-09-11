234cd6bac55a0aaaec919678c880a5dc57e609ac
kyo1506 <vinicius_pinheiro02@hotmail.com>
2021-09-10 22:00:45 -0300

    Add Project API RESTful

--- live/src/main/java/com/dio/live/live/swagger/SwaggerConfig.java
+++ live/src/main/java/com/dio/live/live/swagger/SwaggerConfig.java
@@ -21,8 +21,8 @@ public class SwaggerConfig {
     public Docket apiAdmin() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
-                .apis(RequestHandlerSelectors.basePackage("com.dio.live"))
-                .paths(PathSelectors.ant("/**"))
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.any())
                 .build()
                 .apiInfo(apiInfo());
     }
